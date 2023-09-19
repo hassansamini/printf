b469be280cefd07b57f44ecbd6c1ff55e975d9be
Oycctem <Ousstemm@gmail.com>
2023-09-19 04:18:13 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,53 +1,137 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
+/**
+ *CharPrint - prints a char
+ *@kind : types of arguments
+ *Return: returns nothing
+ */
+int CharPrint(va_list kind)
+{
+	char c;
 
-int _printf(const char *format, ...);
-
-int main() {
-    int count = _printf("Hello, %s! The character is %c and this is a %%.\n", "John", 'A');
-    printf("Total characters printed: %d\n", count);
-    return 0;
+	c = va_arg(kind, int);
+	_putchar(c);
+	return (0);
 }
+/**
+ *StringPrint - prints a string
+ *@kind: types of arguments
+ *Return: returns the number of chars printed
+ */
+int StringPrint(va_list kind)
+{
+	int lenght = 0;
+	char *string;
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+	string = va_arg(kind, char *);
+	if (string == NULL)
+		string = "(null)";
 
-    int count = 0;  // Initialize the character count
-
-    while (*format) {
-        if (*format == '%') {
-            format++;  // Move past the '%'
-
-            // Check the conversion specifier
-            switch (*format) {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    count += printf("%s", va_arg(args, char *));
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    // Handle unsupported specifiers
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        } else {a
-            // Output regular characters
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
+	while (*string)
+	{
+		_putchar(*string);
+		string++, lenght++;
+	}
+	return (lenght);
 }
+/**
+ *intPrint - prints an int
+ *@kind: types of arguments
+ *Return: returns the number of integers printed
+ */
+int intPrint(va_list kind)
+{
+	int n = va_arg(kind, int);
+	int digits;
+	int i = 0, count = 0, divisor = 1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		n = -n;
+	}
+	while (n / divisor > 0)
+	{
+		divisor *= 10;
+		i++;
+	}
+	while (i > 0)
+	{
+		divisor /= 10;
+		digits = (n / divisor) % 10;
+		_putchar('0' + digits);
+		count++, i++;
+	}
+	return (count);
+}
+/**
+ *_printf - function that produces output according to a format
+ *@format: the format of string
+ *Return: returns the number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			while (*format == ' ')
+				format++;
+
+			if (*format == 'c' || *format == 's' || *format == 'd' || *format == 'i')
+			{
+				layout types[] = {
+					{"c", CharPrint},
+					{"s", StringPrint},
+					{"d", intPrint},
+					{"i", intPrint},
+					{NULL, NULL}
+				};
+				int i = 0, found = 0;
+
+				while (types[i].n)
+				{
+					if (*(types[i].n) == *format)
+					{
+						count += types[i].f(args);
+						found = 1;
+					}
+					i++;
+				}
+				if (!found)
+				{
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+				}
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+			}
+		}
+			else
+			{
+				_putchar(*format);
+				count++;
+			}
+			format++;
+		}
+		va_end(args);
+		return (count);
+	}
+
--- main.h
+++ main.h
@@ -1,9 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
 /*
  * Header file for all prototypes
- * used in 0x02-functions_nested_loops
+ * struct structure - the structure
+ * @n: char
+ * @f: int
  */
+typedef struct structure
+{
+	char *n;
+	int (*f)(va_list kind);
+} layout;
+
+int _printf(const char *format, ...);
 int _putchar(char c);
+int CharPrint(va_list kind);
+int StringPrint(va_list kind);
+int intPrint(va_list kind);
+int BinaryPrint(va_list n);
+int DecPrint(va_list kind);
 
 #endif
